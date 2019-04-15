58caa6c96c87f1421e98c4426c5a81d34467bf05
Ben <ben.schwyn@gmail.com>
2019-04-15 14:14:40 -0700

    setup script for mysql data and options

--- setup_mysql_and_load_data.sql
+++ setup_mysql_and_load_data.sql
@@ -1,9 +1,10 @@
-CREATE USER '98point62'@'localhost' IDENTIFIED BY 'password';
+CREATE USER '98point6'@'localhost' IDENTIFIED BY 'password';
 GRANT ALL PRIVILEGES ON *.* TO '98point6'@'localhost';
 FLUSH PRIVILEGES;
 
 CREATE DATABASE mydb;
 USE mydb;
+
 CREATE TABLE game_data (
 game_id VARCHAR(255),
 player_id INT,
@@ -12,8 +13,10 @@ column_number INT,
 RESULT VARCHAR(255)
 );
 
-LOAD DATA LOCAL INFILE '~/Projects/droptoken/game_data.csv'
+LOAD DATA LOCAL INFILE './game_data.csv'
 INTO TABLE game_data
 FIELDS TERMINATED BY ','
 LINES TERMINATED BY '\n'
 IGNORE 1 ROWS;
+
+SET GLOBAL sql_mode=(SELECT REPLACE(@@sql_mode,'ONLY_FULL_GROUP_BY',''));
